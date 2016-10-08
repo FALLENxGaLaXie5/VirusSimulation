2b02778289c3100d131ca9af97b9e40d89839942
FALLENxGaLaXie5 <steward.joshua@rocketmail.com>
2016-10-08 00:10:04 -0700

    destinations added, pathfinding working

---
+++ VirusProject/Assets/Scripts/DisplayText.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayText : MonoBehaviour
+{
+    public string destinationTag;
+    public float range;
+    GameObject[] destinations;
+	// Use this for initialization
+	void Start ()
+    {
+        destinations = GameObject.FindGameObjectsWithTag(destinationTag);
+	}
+
+    void Update()
+    {
+        checkIfNearDestination();
+    }
+
+    void checkIfNearDestination()
+    {
+        for (int i = 0; i < destinations.Length; i++)
+        {
+            GameObject currentDestination = destinations[i];
+            float distance = Vector3.Distance(transform.position, currentDestination.transform.position);
+            if (distance < range)
+            {
+                destinationTalk talkScript = currentDestination.GetComponent<destinationTalk>();
+                talkScript.talk = true;
+            }
+            else
+            {
+                destinationTalk talkScript = currentDestination.GetComponent<destinationTalk>();
+                talkScript.talk = false;
+            }
+        }
+    }
+}
---
+++ VirusProject/Assets/Scripts/destinationTalk.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destinationTalk : MonoBehaviour
+{
+    public bool talk;
+    // Use this for initialization
+    TextMesh text;
+    GameObject text1;
+    void Start ()
+    {
+        text = gameObject.GetComponentInChildren(typeof(TextMesh)) as TextMesh;
+        talk = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(text != null)
+        {
+            if (text.gameObject.tag == "coffeeTag" && talk)
+            {
+                text.text = "I'm a coffee shop!";
+            }
+            else if (text.gameObject.tag == "foodTag" && talk)
+            {
+                text.text = "I gotz food for ya!";
+            }
+            else if (talk == false)
+            {
+                text.text = "";
+            }
+        }
+	}
+}
