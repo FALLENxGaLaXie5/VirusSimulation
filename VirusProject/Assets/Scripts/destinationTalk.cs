8a60ef152516fd193ee7d6652a74f8d7fda21d3c
FALLENxGaLaXie5 <steward.joshua@rocketmail.com>
2016-10-08 22:55:39 -0700

    lots of destinations

--- VirusProject/Assets/Scripts/destinationTalk.cs
+++ VirusProject/Assets/Scripts/destinationTalk.cs
@@ -26,6 +26,30 @@ public class destinationTalk : MonoBehaviour
             {
                 text.text = "I gotz food for ya!";
             }
+            else if(text.gameObject.tag == "schoolTag" && talk)
+            {
+                text.text = "School time bois and gurls!";
+            }
+            else if(text.gameObject.tag == "churchTag" && talk)
+            {
+                text.text = "Come get ya Jesus!";
+            }
+            else if (text.gameObject.tag == "clubTag" && talk)
+            {
+                text.text = "UNGST UNGST UNGST UNGST UNGST UNGST UNGST";
+            }
+            else if (text.gameObject.tag == "frysElectronicsTag" && talk)
+            {
+                text.text = "Electronics for everyone!";
+            }
+            else if (text.gameObject.tag == "homeTag" && talk)
+            {
+                text.text = "Finally home...";
+            }
+            else if (text.gameObject.tag == "gameStopTag" && talk)
+            {
+                text.text = "ALL DA GAMES!";
+            }
             else if (talk == false)
             {
                 text.text = "";
