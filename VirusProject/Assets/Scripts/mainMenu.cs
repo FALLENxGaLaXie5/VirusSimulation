fd53b6dd5adaa619b5279fdc65da0383130d6eb6
FALLENxGaLaXie5 <steward.joshua@rocketmail.com>
2016-10-19 20:18:22 -0700

    mainMenu

---
+++ VirusProject/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	/*void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}*/
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
