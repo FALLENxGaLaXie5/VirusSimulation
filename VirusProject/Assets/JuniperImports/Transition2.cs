88965c97dd11c59ef471ed4b154d291823170f3f
FALLENxGaLaXie5 <steward.joshua@rocketmail.com>
2016-11-06 05:20:57 -0800

    animation/menu changes

---
+++ VirusProject/Assets/JuniperImports/Transition2.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Transition2 : MonoBehaviour
+{
+    public Canvas quitMenu;
+    //public Button startText;
+    //public Button exitText;
+
+    void Start()
+    {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        //startText = startText.GetComponent<Button>();
+        //exitText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            ExitPress();
+        }
+    }
+
+    public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        //startText.enabled = false;
+        //exitText.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        quitMenu.enabled = false;
+        //startText.enabled = true;
+        //exitText.enabled = true;
+    }
+
+    public void transitionScene()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
