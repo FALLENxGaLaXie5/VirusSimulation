c1632259d02f3a3e1a60e3ca2f7975b185b5bb43
FALLENxGaLaXie5 <steward.joshua@rocketmail.com>
2016-11-16 20:00:58 -0800

    more builds!

---
+++ VirusProject/Assets/Scripts/transHelp.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class transHelp : MonoBehaviour
+{
+    public void transTitle()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
